72b8513c12b99c2d1d38f83c213cf00100a7f5c6
skhoct30 <skhoct30@naver.com>
2025-05-27 18:51:20 +0900

    [서강현] - 예제 반복문 추가

--- src/main/java/com/skhoct30/spring/ex/thymeleaf/ThymeleafController.java
+++ src/main/java/com/skhoct30/spring/ex/thymeleaf/ThymeleafController.java
@@ -1,14 +1,35 @@
 package com.skhoct30.spring.ex.thymeleaf;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/thymeleaf")
 public class ThymeleafController {
 	
-	@GetMapping("/thymeleaf/ex01")
+	@GetMapping("/ex01")
 	public String ex01() {
 		return "thymeleaf/ex01";
+	}
+	
+	@GetMapping("/ex02")
+	public String ex02(Model model) {
+		
+		// 과일 이름 저장하는 List
+		List<String> fruitList = new ArrayList<>();	
+		
+		fruitList.add("사과");
+		fruitList.add("귤");
+		fruitList.add("바나나");
+		
+		model.addAttribute("fruitList", fruitList);
+		
+		return "thymeleaf/ex02";	
 	}
 	
 }
